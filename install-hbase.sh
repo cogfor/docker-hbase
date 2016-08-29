2d9d83dd3a966839a4d762c4f864c748ed7078e5
Rene Nederhand <rene@cogfor.com>
2016-08-29 17:14:34 +0200

    fix filename typo

--- install-hbase.sh
+++ install-hbase.sh
@@ -2,5 +2,5 @@
 
 wget --quiet http://apache.proserve.nl/hbase/0.98.21/hbase-0.98.21-hadoop2-bin.tar.gz
 tar xzf hbase-0.98.21-hadoop2-bin.tar.gz -C /opt/
-ln -s hbase-0.98.21-hadoop2-bin.tar.gz /opt/hbase
+ln -s hbase-0.98.21-hadoop2 /opt/hbase
 rm hbase-0.98.21-hadoop2-bin.tar.gz
